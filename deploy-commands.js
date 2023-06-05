a103e33b0dcc29c6fdb5898b97fe1e6e5b29a39e
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-06-05 04:20:53 +0000

    Switched to .env

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const { REST, Routes } = require('discord.js');
 const config = require("./config.json");
 const clientId = config.client.id;
 const guildId = config.guild;
-const token = config.client.token;
+require('dotenv').config();
 const fs = require('node:fs');
 
 const commands = [];
@@ -16,7 +16,7 @@ for (const file of commandFiles) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 // and deploy your commands!
 (async () => {
